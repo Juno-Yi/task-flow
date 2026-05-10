0b2e048828951319c934e1ee862f39702ede2225
EatFan <eatfan0921@163.com>
2026-05-10 15:41:51 +0800

    feat(task): 添加任务管理控制器
    - 实现任务列表分页查询接口
    - 添加新增任务功能接口
    - 实现修改任务功能接口
    - 集成权限控制注解配置
    - 添加平台作用域安全控制

---
+++ junoyi-module/junoyi-module-task/src/main/java/com/junoyi/task/controller/TaskListController.java
@@ -0,0 +1,58 @@
+package com.junoyi.task.controller;
+
+
+import com.junoyi.framework.core.domain.module.R;
+import com.junoyi.framework.core.domain.page.PageResult;
+import com.junoyi.framework.permission.annotation.Permission;
+import com.junoyi.framework.security.annotation.PlatformScope;
+import com.junoyi.framework.security.enums.PlatformType;
+import com.junoyi.framework.web.domain.BaseController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 任务管理控制器
+ *
+ * @author Fan
+ */
+@RestController
+@RequestMapping("/task/list")
+@RequiredArgsConstructor
+public class TaskListController extends BaseController {
+
+    /**
+     * 获取任务管理列表（分页）
+     */
+    @GetMapping("/list")
+    @PlatformScope(PlatformType.ADMIN_WEB)
+    @Permission(
+            value = "task.ui.list.view"
+    )
+    public R<PageResult<?>> getTaskList(){
+        return R.ok();
+    }
+
+    /**
+     * 添加任务
+     */
+    @PostMapping
+    @PlatformScope(PlatformType.ADMIN_WEB)
+    @Permission(
+            value = "task.ui.list.button.add"
+    )
+    public R<Void> addTask(){
+        return R.ok();
+    }
+
+    /**
+     * 修改任务
+     */
+    @PutMapping
+    @PlatformScope(PlatformType.ADMIN_WEB)
+    @Permission(
+            value = "task.ui.list.button.edit"
+    )
+    public R<Void> updateTask(){
+        return R.ok();
+    }
+}
