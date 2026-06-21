be1ea0828ef0bc1def6723816da3bf562642292c
EatFan <eatfan0921@163.com>
2026-06-21 16:17:19 +0800

    feat(notification): 添加我的通知控制器
    - 创建了 MyNotificationController 类用于处理通知相关请求
    - 实现了获取我的通知消息列表接口
    - 集成了分页结果返回功能
    - 添加了平台作用域注解支持管理员端访问
    - 继承基础控制器类提供了通用功能支持

---
+++ junoyi-module/junoyi-module-notification/src/main/java/com/junoyi/notification/controller/MyNotificationController.java
@@ -0,0 +1,34 @@
+package com.junoyi.notification.controller;
+
+import com.junoyi.framework.core.domain.module.R;
+import com.junoyi.framework.core.domain.page.PageResult;
+import com.junoyi.framework.security.annotation.PlatformScope;
+import com.junoyi.framework.security.enums.PlatformType;
+import com.junoyi.framework.web.domain.BaseController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ *  我的通知控制器
+ *
+ * @author Fan
+ */
+@RestController
+@RequestMapping("/notification/my-notification")
+@RequiredArgsConstructor
+public class MyNotificationController extends BaseController {
+
+    /**
+     * 获取我的通知消息列表
+     */
+    @GetMapping("/list")
+    @PlatformScope(PlatformType.ADMIN_WEB)
+    public R<PageResult<?>> getMyNotificationList(){
+
+        return R.ok();
+    }
+}
